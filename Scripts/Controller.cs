598eb11bcb7e12df50eae65dd1b4f39341c5bf9f
Chaumartin Léo <800031-lchaumartin@users.noreply.gitlab.com>
2021-01-21 02:32:11 +0100

    added controller

---
+++ Scripts/Controller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Controller : MonoBehaviour
+{
+    public float _speed = 1f;
+
+    private Rigidbody _rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float multiplier = 1f;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            multiplier = 2f;
+            GetComponentInChildren<ProceduralAnimation>().running = true;
+        }
+        else
+            GetComponentInChildren<ProceduralAnimation>().running = false;
+
+        if (_rigidbody.velocity.magnitude < _speed * multiplier)
+        {
+
+            float value = Input.GetAxis("Vertical");
+            if (value != 0)
+                _rigidbody.AddForce(0, 0, value * Time.fixedDeltaTime * 1000f);
+            value = Input.GetAxis("Horizontal");
+            if (value != 0)
+                _rigidbody.AddForce(value * Time.fixedDeltaTime * 1000f, 0f, 0f);
+        }
+    }
+}
